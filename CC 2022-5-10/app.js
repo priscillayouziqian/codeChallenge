ee770ed17d965de593b5b50915eab0f1b3b4abd7
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-05-09 23:13:51 -0400

    Add solutions/returns to coding challenge from 5-10

--- CC 2022-5-10/app.js
+++ CC 2022-5-10/app.js
@@ -26,3 +26,7 @@ let humanYearsCatYearsDogYears = function(humanYears){
      return [humanYears, (humanYears-2) * 4 + (15+9), (humanYears-2) * 5 + (15+9)]
      
 }
+
+console.log(humanYearsCatYearsDogYears(1)) //should return [1,15,15]
+console.log(humanYearsCatYearsDogYears(2)) //should return [2, 24, 24]
+console.log(humanYearsCatYearsDogYears(3)) //should return [3, 28, 29]
