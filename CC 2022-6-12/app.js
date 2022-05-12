3d66e81537606e82bd9469ed9413f8861da369f3
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-05-11 23:08:23 -0400

    Add some changes

--- CC 2022-6-12/app.js
+++ CC 2022-6-12/app.js
@@ -3,8 +3,8 @@
 // Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
 
 function unusualFive(){
-    //calculate the string's length can avoid ban characters.
-    //the string must have 5 letter's length.
+    //calculate the string's length can avoid ban characters, the string must have 5 letter's length.
+    
     return 'fifth'.length
 }
 
