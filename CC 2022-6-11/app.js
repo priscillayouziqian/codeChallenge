023176de9dcfdd1debbdd467923d2a8c11dafe5b
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-05-15 21:44:59 -0400

    Add second coding challenge of the day

--- CC 2022-6-11/app.js
+++ CC 2022-6-11/app.js
@@ -11,3 +11,22 @@ sayHello('Mr. Spock') // Hello, Mr. Spock
 sayHello("Captain Kirk") // Hello, Captain Kirk
 sayHello("Dr. McCoy") //Hello, Dr. McCoy
 
+// Triple Trouble
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+// Note: You can expect all of the inputs to be the same length.
+
+function tripleTrouble(one, two, three){
+    let str = '' //declare a variable to store the strings
+    //empty varialbe needs to create outside of for loop, or it won't be regconize when return at the end
+    for (i=0; i<one.length; i++){
+        str += one[i]+two[i]+three[i]
+    }
+    return str
+}
+
+tripleTrouble('aaa', 'bbb', 'ccc') //abcabcabc
+tripleTrouble("aaaaaa","bbbbbb","cccccc") //abcabcabcabcabcabc
+tripleTrouble("burn", "reds", "roll") //brrueordlnsl
