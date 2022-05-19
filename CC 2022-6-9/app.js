1dd8ede2996ca92f0c2e0f81c34ea4a5adac670f
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-05-18 23:09:38 -0400

    Add changes to coding challenge from 6-9

--- CC 2022-6-9/app.js
+++ CC 2022-6-9/app.js
@@ -11,9 +11,16 @@ seatsInTheater(nCols, nRows, col, row) === 96
 [output] an integer
 The number of people who sit strictly behind you and in your column or to the left.*/
 
-function seatInTheater(nCols, nRows, col, row){
+function seatsInTheater(nCols, nRows, col, row){
     //let's say the value we need to get is the area of rectangle.
     //nCols - col + 1 is the length, and nRows - row is the width.
     //rectangle area = length * width
     return (nCols - col + 1) * (nRows - row)
 }
+
+//Examples:
+seatsInTheater(16,11,5,3) //96
+
+seatsInTheater(1,1,1,1) //0
+
+seatsInTheater(13,6,8,3) //18
