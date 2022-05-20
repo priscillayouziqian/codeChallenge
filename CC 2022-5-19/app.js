9e5ac682f24438b9435f1a0f57d0ed2ced9ed54f
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-05-19 22:50:47 -0400

    Add edge cases to coding challenge from 5-19.

--- CC 2022-5-19/app.js
+++ CC 2022-5-19/app.js
@@ -4,3 +4,18 @@
 // Example:
 // 348597 => [7,9,5,8,4,3]
 // 0 => [0]
+
+//Parameter: are you ever given an empty array or undefined? what them? are you always given digits?
+//Return: whatever number was given, it is now split up into an array and the order of the numbers is reversed.
+//Example: if we were given 934 => ['4', '3', '9']
+//                          234 => ['4', '3', '2']
+//                          undefined => not a number
+//P:make a function that takes in a number
+function reverseArray(num){
+    let numArray = num.toString().split('').reverse()
+    //reverse array, turn array intergers again and return
+    return numArray.map(e=>parseInt(e))
+
+}
+
+reverseArray(934)//should return [4,3,9]
