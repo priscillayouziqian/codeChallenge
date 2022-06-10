79dae36436b4cc1032fea39393835c495b2a3ec1
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-06-09 23:05:18 -0400

    Coding challenge for 6-9

---
+++ CC 2022-6-9/app.js
@@ -0,0 +1,19 @@
+/*
+
+Given the total number of rows and columns in the theater (nRows and nCols, respectively), and the row and column you're sitting in, return the number of people who sit strictly behind you and in your column or to the left, assuming all seats are occupied.
+
+Example
+For nCols = 16, nRows = 11, col = 5 and row = 3, the output should be
+
+seatsInTheater(nCols, nRows, col, row) === 96
+
+
+[output] an integer
+The number of people who sit strictly behind you and in your column or to the left.*/
+
+function seatInTheater(nCols, nRows, col, row){
+    //let's say the value we need to get is the area of rectangle.
+    //nCols - col + 1 is the length, and nRows - row is the width.
+    //rectangle area = length * width
+    return (nCols - col + 1) * (nRows - row)
+}
