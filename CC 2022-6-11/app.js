dbe2a6a0fcfd033d31961b554663c540716480f6
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-06-11 21:24:08 -0400

    Coding challenge for 6-11

---
+++ CC 2022-6-11/app.js
@@ -0,0 +1,13 @@
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+function sayHello(name){
+    return `Hello, ${name}` //using ``template to put name parameter in the greetings. Remember to pay attention to the tiny details, like ,
+}
+
+// Example output:
+
+sayHello('Mr. Spock') // Hello, Mr. Spock
+sayHello("Captain Kirk") // Hello, Captain Kirk
+sayHello("Dr. McCoy") //Hello, Dr. McCoy
+
