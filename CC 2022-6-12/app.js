5b597f6bf3f3cedb19c5d6fe122107695ac8d18d
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-06-12 23:05:55 -0400

    Coding challenge for 6-12

---
+++ CC 2022-6-12/app.js
@@ -0,0 +1,11 @@
+//Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+function unusualFive(){
+    //calculate the string's length can avoid ban characters.
+    //the string must have 5 letter's length.
+    return 'fifth'.length
+}
+
+unusualFive()
