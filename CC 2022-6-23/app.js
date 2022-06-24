3ab775ba13eab2592747a46736a318cc6ea131bc
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-06-23 21:37:35 -0400

    Coding challenge for 6-23

---
+++ CC 2022-6-23/app.js
@@ -0,0 +1,6 @@
+// create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+    return str.slice(1, -1)
+    //apply prototype slice(beginIndex, endIndex), begins from str[1] because removed the first character; end with str[-1] because remove the last character.
+}
