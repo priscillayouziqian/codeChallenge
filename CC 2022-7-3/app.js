65ef8dc196af3575643c598d5fec7e3b94b5c659
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-07-02 18:04:59 -0400

    Add another solution to coding challenge 7-3

--- CC 2022-7-3/app.js
+++ CC 2022-7-3/app.js
@@ -4,6 +4,16 @@ function combat(health, damage){
     return health-damage>=0 ? health-damage : 0
 }
 
-combat(100,5) //return 5
+combat(100,5) //return 95
 combat(92, 8) //return 84
 combat(20, 30) //return 0
+
+//add another solution
+
+function combat2(health, damage){
+    return health<damage ? 0 : health-damage
+}
+
+combat2(100,5) //return 95
+combat2(92, 8) //return 84
+combat2(20, 30) //return 0
