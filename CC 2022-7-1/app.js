2b5c5f9b69154b9b27b55c17c9a1ef6f6ca634a8
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-08-08 22:46:10 -0400

    coding challenge for 8-8

--- CC 2022-7-1/app.js
+++ CC 2022-7-1/app.js
@@ -12,3 +12,28 @@ century(1705) //--> 18
 century(1900) //--> 19
 century(1601) //--> 17
 century(2000) //--> 20
+
+class Dog {
+    constructor(name, color){
+        this.name = name;
+        this.color = color;
+      }
+  
+      bark(){
+        console.log('Arf! I am', this.name, 'the dog!');
+      }
+  }
+  
+  class Puppy extends Dog {
+    constructor(name, color, trainingLevel) {
+      super(name, color)
+  
+      this.trainingLevel = trainingLevel
+    }
+  
+    levelUp(num) {
+      this.trainingLevel += num
+    }
+  }
+
+  Puppy.bark();
